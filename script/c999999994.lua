5b2a30ae1b431b72dd586eabb9f8c6826580bf6d
necrodraco <ygohameln@gmail.com>
2018-03-07 23:45:40 +0100

    Fixing Missus Radiant and Friends

--- script/c999999994.lua
+++ script/c999999994.lua
@@ -1,16 +1,18 @@
---ミセス・レディエント
---Missus Radiant
+--ドリトル・キメラ
+--
+--do-little Chimera
+--Scripted by Eerie Code
 --reused by Necrodraco
-function c999999995.initial_effect(c)
+function c999999994.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcFunRep(c,c999999995.ffilter,2,false)
+	aux.AddFusionProcFunRep(c,c999999994.ffilter,2,false)
 	--spsummon condition
 	local e6=Effect.CreateEffect(c)
 	e6:SetType(EFFECT_TYPE_SINGLE)
 	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e6:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e6:SetValue(c999999995.splimit)
+	e6:SetValue(c999999994.splimit)
 	c:RegisterEffect(e6)
 	--special summon rule
 	local e7=Effect.CreateEffect(c)
@@ -18,8 +20,8 @@ function c999999995.initial_effect(c)
 	e7:SetCode(EFFECT_SPSUMMON_PROC)
 	e7:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e7:SetRange(LOCATION_EXTRA)
-	e7:SetCondition(c999999995.spcon)
-	e7:SetOperation(c999999995.spop)
+	e7:SetCondition(c999999994.spcon)
+	e7:SetOperation(c999999994.spop)
 	c:RegisterEffect(e7)
 	--atk/def
 	local e1=Effect.CreateEffect(c)
@@ -27,14 +29,14 @@ function c999999995.initial_effect(c)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_EARTH))
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE))
 	e1:SetValue(500)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e2)
 	local e3=e1:Clone()
-	e3:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WIND))
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER))
 	e3:SetValue(-400)
 	c:RegisterEffect(e3)
 	local e4=e3:Clone()
@@ -42,55 +44,56 @@ function c999999995.initial_effect(c)
 	c:RegisterEffect(e4)
 	--tohand
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(999999995,0))
+	e5:SetDescription(aux.Stringid(999999994,0))
 	e5:SetCategory(CATEGORY_TOHAND)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e5:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e5:SetCode(EVENT_DESTROYED)
-	e5:SetCountLimit(1,999999995)
-	e5:SetCondition(c999999995.thcon)
-	e5:SetTarget(c999999995.thtg)
-	e5:SetOperation(c999999995.thop)
+	e5:SetCountLimit(1,999999994)
+	e5:SetCondition(c999999994.thcon)
+	e5:SetTarget(c999999994.thtg)
+	e5:SetOperation(c999999994.thop)
 	c:RegisterEffect(e5)
 end
-function c999999995.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c999999994.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
 end
-function c999999995.thfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToHand()
+function c999999994.thfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsAbleToHand()
 end
-function c999999995.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c999999995.thfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c999999995.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+function c999999994.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c999999994.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c999999994.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,c999999995.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,c999999994.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
-function c999999995.thop(e,tp,eg,ep,ev,re,r,rp)
+function c999999994.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
 
+
 --Borrowed Code of Starving Venom Pendulum Contact Fusion
-function c999999995.ffilter(c)
-	return c:IsFusionAttribute(ATTRIBUTE_EARTH)
+function c999999994.ffilter(c)
+	return c:IsFusionAttribute(ATTRIBUTE_FIRE)
 end
-function c999999995.splimit(e,se,sp,st)
+function c999999994.splimit(e,se,sp,st)
 	return bit.band(st,SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
 end
-function c999999995.spfilter(c,fc)
-	return c999999995.ffilter(c) and c:IsCanBeFusionMaterial(fc)
+function c999999994.spfilter(c,fc)
+	return c999999994.ffilter(c) and c:IsCanBeFusionMaterial(fc)
 end
-function c999999995.spcon(e,c)
+function c999999994.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
-		and Duel.CheckReleaseGroup(tp,c999999995.spfilter,2,nil,c)
+		and Duel.CheckReleaseGroup(tp,c999999994.spfilter,2,nil,c)
 end
-function c999999995.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,c999999995.spfilter,2,2,nil,c)
+function c999999994.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,c999999994.spfilter,2,2,nil,c)
 	c:SetMaterial(g)
 	Duel.Release(g,REASON_COST+REASON_FUSION+REASON_MATERIAL)
 end
