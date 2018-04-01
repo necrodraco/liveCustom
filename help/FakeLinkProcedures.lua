ea5bfb38a70e47d03052ea03ededdb203b4b5148
NecroDraco <ygohameln@gmail.com>
2018-04-01 02:16:37 +0200

    Updating FakeLinkProcedure and adding comments for beginners

--- help/FakeLinkProcedures.lua
+++ help/FakeLinkProcedures.lua
@@ -3,67 +3,56 @@ function cXXXXXXXXX.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcFunRep(c,cXXXXXXXXX.matfilter,2,false)
+	--spsummon condition
+	local e100=Effect.CreateEffect(c)
+	e100:SetType(EFFECT_TYPE_SINGLE)
+	e100:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e100:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e100:SetValue(cXXXXXXXXX.splimit)
+	c:RegisterEffect(e100)
 	--special summon rule
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_FIELD)
-	e7:SetCode(EFFECT_SPSUMMON_PROC)
-	e7:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e7:SetRange(LOCATION_EXTRA)
-	e7:SetCondition(cXXXXXXXXX.spcon)
-	e7:SetOperation(cXXXXXXXXX.spop)
-	c:RegisterEffect(e7)
+	local e99=Effect.CreateEffect(c)
+	e99:SetType(EFFECT_TYPE_FIELD)
+	e99:SetCode(EFFECT_SPSUMMON_PROC)
+	e99:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e99:SetRange(LOCATION_EXTRA)
+	e99:SetCondition(cXXXXXXXXX.spcon)
+	e99:SetOperation(cXXXXXXXXX.spop)
+	c:RegisterEffect(e99)
 	--It will automatically set to ATK. But it can be set facedown at the moment - TODO
-	local e8=Effect.CreateEffect(c)
-	e8:SetType(EFFECT_TYPE_FIELD)
-	e8:SetCode(EFFECT_SET_POSITION)
-	e8:SetRange(LOCATION_MZONE)
-	e8:SetTarget(cXXXXXXXXX.target)
-	e8:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e8:SetValue(POS_FACEUP_ATTACK)
-	c:RegisterEffect(e8)
+	local e98=Effect.CreateEffect(c)
+	e98:SetType(EFFECT_TYPE_FIELD)
+	e98:SetCode(EFFECT_SET_POSITION)
+	e98:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e98:SetRange(LOCATION_MZONE)
+	e98:SetTarget(cXXXXXXXXX.target)
+	e98:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e98:SetValue(POS_FACEUP_ATTACK)
+	c:RegisterEffect(e98)
 end
 
---Borrowed Code of Starving Venom Pendulum Contact Fusion
+--Borrowed Code of Starving Venom Pendulum Contact Fusion and Chimeratech Fortress Dragon
 function cXXXXXXXXX.matfilter(c,scard,sumtype,tp)
-	return c:IsType(TYPE_FIEND,scard,sumtype,tp)
+	return -- summon condition. e.g. "c:IsType(TYPE_FIEND,scard,sumtype,tp)" for "n Fiend-Type Monster"
+end
+function cXXXXXXXXX.splimit(e,se,sp,st)
+	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
 end
 function cXXXXXXXXX.spfilter(c,fc)
-	return cXXXXXXXXX.matfilter(c) and c:IsCanBeFusionMaterial(fc)
+	return cXXXXXXXXX.matfilter(c) and c:IsCanBeFusionMaterial(fc) and (c:IsControler(tp) or c:IsFaceup())
 end
 function cXXXXXXXXX.spcon(e,c)
 	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
-		and Duel.CheckReleaseGroup(tp,cXXXXXXXXX.spfilter,2,nil,c)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return ft>-2--Max Count of Monsters
+		and Duel.IsExistingMatchingCard(cXXXXXXXXX.spfilter,tp,LOCATION_ONFIELD,nil,2,nil,tp,ft)--the Number 2 represents the Max Count of Monsters
 end
 function cXXXXXXXXX.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,cXXXXXXXXX.spfilter,2,2,nil,c)
-	c:SetMaterial(g)
-	Duel.Release(g,REASON_COST+REASON_FUSION+REASON_MATERIAL)
+	local g=Duel.GetMatchingGroup(cXXXXXXXXX.spfilter,tp,LOCATION_MZONE,nil,nil,tp)
+	local g1=g:Select(tp,2,2,nil)--Count of Monsters thats needed. first is minimal count, second ist maximal count
+	Duel.SendtoGrave(g1,REASON_COST)
 end
 --Borrowed from Area A
 function cXXXXXXXXX.target(e,c)
 	return c:GetCode() == XXXXXXXXX
-end
-
-
-
-
---Alternate from Blue-Eyes Twin Burst Dragon
---青眼の双爆裂龍
-function cXXXXXXXXX.initial_effect(c)
-	--fusion material
-	c:EnableReviveLimit()
-	aux.AddFusionProcMixN(c,true,true,89631139,2)
-	aux.AddContactFusion(c,cXXXXXXXXX.contactfil,cXXXXXXXXX.contactop,cXXXXXXXXX.splimit)
-end
-cXXXXXXXXX.material_setcode=0xdd
-function cXXXXXXXXX.splimit(e,se,sp,st)
-	return bit.band(st,SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
-end
-function cXXXXXXXXX.contactfil(tp)
-	return Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,nil)
-end
-function cXXXXXXXXX.contactop(g)
-	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
 end
