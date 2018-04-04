b5c8cf85146d9478f6ac36cdd45112d12526920f
necrodraco <ygohameln@gmail.com>
2018-04-04 23:43:07 +0200

    Adding new Brandish girl

---
+++ script/c999999914.lua
@@ -0,0 +1,93 @@
+--閃刀姫-ハヤテ
+--Brandish Maiden Hayate
+--Scripted by ahtelel
+function c999999914.initial_effect(c)
+	c:SetSPSummonOnce(999999914)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcFunRep(c,c999999914.ffilter,2,false)
+	--spsummon condition
+	local e100=Effect.CreateEffect(c)
+	e100:SetType(EFFECT_TYPE_SINGLE)
+	e100:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e100:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e100:SetValue(c999999914.splimit)
+	c:RegisterEffect(e100)
+	--special summon rule
+	local e99=Effect.CreateEffect(c)
+	e99:SetType(EFFECT_TYPE_FIELD)
+	e99:SetCode(EFFECT_SPSUMMON_PROC)
+	e99:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e99:SetRange(LOCATION_EXTRA)
+	e99:SetCondition(c999999914.spcon)
+	e99:SetOperation(c999999914.spop)
+	e99:SetValue(SUMMON_TYPE_FUSION)
+	c:RegisterEffect(e99)
+	--It will automatically set to ATK. But it can be set facedown at the moment - TODO
+	local e98=Effect.CreateEffect(c)
+	e98:SetType(EFFECT_TYPE_FIELD)
+	e98:SetCode(EFFECT_SET_POSITION)
+	e98:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SET_AVAILABLE)
+	e98:SetRange(LOCATION_MZONE)
+	e98:SetTarget(c999999914.target)
+	e98:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e98:SetValue(POS_FACEUP_ATTACK)
+	c:RegisterEffect(e98)
+	--direct attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e1)
+	--to grave
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(999999914,0))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLED)
+	e2:SetTarget(c999999914.gytg)
+	e2:SetOperation(c999999914.gyop)
+	c:RegisterEffect(e2)
+end
+function c999999914.matfilter(c,scard,sumtype,tp)
+	return c:IsFusionSetCard(0x1115) and not c:IsAttribute(ATTRIBUTE_WIND,scard,sumtype,tp)
+end
+function c999999914.tgfilter(c)
+	return c:IsSetCard(0x115) and c:IsAbleToGrave()
+end
+function c999999914.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c999999914.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function c999999914.gyop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c999999914.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
+--Borrowed Code of Starving Venom Pendulum Contact Fusion
+function c999999914.ffilter(c)
+	return c999999914.matfilter--c:IsFusionAttribute(ATTRIBUTE_EARTH)
+end
+function c999999914.splimit(e,se,sp,st)
+	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+end
+function c999999914.spfilter(c,fc)
+	return c999999914.matfilter(c)
+end
+function c999999914.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return ft>-1
+		and Duel.IsExistingMatchingCard(c999999914.spfilter,tp,LOCATION_ONFIELD,nil,1,nil,tp,ft)
+end
+function c999999914.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetMatchingGroup(c999999914.spfilter,tp,LOCATION_MZONE,nil,nil,tp)
+	local g1=g:Select(tp,1,1,nil)
+	Duel.SendtoGrave(g1,REASON_COST)
+end
+--Borrowed from Area A
+function c999999914.target(e,c)
+	return c:GetCode() == 999999914
+end
