2e1547dd15583e974b5dcd3911f15736f0d7e25f
necrodraco <ygohameln@gmail.com>
2018-04-16 22:51:26 +0200

    Quick Fix of Cerberus

--- script/c999999927.lua
+++ script/c999999927.lua
@@ -2,21 +2,48 @@
 --Troymare Cerberus
 --Script by nekrozar
 --reused by NecroDraco
-function c75452921.initial_effect(c)
-	--link summon
-	aux.AddLinkProcedure(c,nil,2,2,c75452921.lcheck)
+function c999999927.initial_effect(c)
+	--fusion material
 	c:EnableReviveLimit()
+	aux.AddFusionProcFunRep(c,c999999927.matfilter,2,false)
+	--spsummon condition
+	local e100=Effect.CreateEffect(c)
+	e100:SetType(EFFECT_TYPE_SINGLE)
+	e100:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e100:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e100:SetValue(c999999927.splimit)
+	c:RegisterEffect(e100)
+	--special summon rule
+	local e99=Effect.CreateEffect(c)
+	e99:SetType(EFFECT_TYPE_FIELD)
+	e99:SetCode(EFFECT_SPSUMMON_PROC)
+	e99:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e99:SetRange(LOCATION_EXTRA)
+	e99:SetCondition(c999999927.spcon)
+	e99:SetOperation(c999999927.spop)
+	e99:SetValue(SUMMON_TYPE_FUSION)
+	c:RegisterEffect(e99)
+	--It will automatically set to ATK. But it can be set facedown at the moment - TODO
+	local e98=Effect.CreateEffect(c)
+	e98:SetType(EFFECT_TYPE_FIELD)
+	e98:SetCode(EFFECT_SET_POSITION)
+	e98:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SET_AVAILABLE)
+	e98:SetRange(LOCATION_MZONE)
+	e98:SetTarget(c999999927.target)
+	e98:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e98:SetValue(POS_FACEUP_ATTACK)
+	c:RegisterEffect(e98)
 	--destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(75452921,0))
+	e1:SetDescription(aux.Stringid(999999927,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCountLimit(1,75452921)
-	e1:SetCondition(c75452921.descon)
-	e1:SetCost(c75452921.descost)
-	e1:SetTarget(c75452921.destg)
-	e1:SetOperation(c75452921.desop)
+	e1:SetCountLimit(1,999999927)
+	e1:SetCondition(c999999927.descon)
+	e1:SetCost(c999999927.descost)
+	e1:SetTarget(c999999927.destg)
+	e1:SetOperation(c999999927.desop)
 	c:RegisterEffect(e1)
 	--[[indes
 	local e2=Effect.CreateEffect(c)
@@ -24,28 +51,28 @@ function c75452921.initial_effect(c)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(c75452921.indtg)
+	e2:SetTarget(c999999927.indtg)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)]]
 end
-function c75452921.lcheck(g,lc)
+function c999999927.lcheck(g,lc)
 	return g:GetClassCount(Card.GetCode)==g:GetCount()
 end
-function c75452921.descon(e,tp,eg,ep,ev,re,r,rp)
+function c999999927.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
-function c75452921.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c999999927.descost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
-function c75452921.desfilter(c)
+function c999999927.desfilter(c)
 	return c:IsSummonType(SUMMON_TYPE_SPECIAL) --and c:GetSequence()<5
 end
-function c75452921.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and c75452921.desfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c75452921.desfilter,tp,0,LOCATION_MZONE,1,nil) end
+function c999999927.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and c999999927.desfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c999999927.desfilter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c75452921.desfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,c999999927.desfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	--if e:GetHandler():GetMutualLinkedGroupCount()>0 then
 		e:SetLabel(1)
@@ -56,16 +83,45 @@ function c75452921.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if e:GetLabel()==1 then cat=cat+CATEGORY_DRAW end
 	e:SetCategory(cat)
 end
-function c75452921.desop(e,tp,eg,ep,ev,re,r,rp)
+function c999999927.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0
 		and e:GetLabel()==1 and Duel.IsPlayerCanDraw(tp,1)
-		and Duel.SelectYesNo(tp,aux.Stringid(75452921,1)) then
+		and Duel.SelectYesNo(tp,aux.Stringid(999999927,1)) then
 		Duel.BreakEffect()
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end
---[[function c75452921.indtg(e,c)
+--[[function c999999927.indtg(e,c)
 	return c:GetMutualLinkedGroupCount()>0
 end]]
 
+--Borrowed Code of Starving Venom Pendulum Contact Fusion and Chimeratech Fortress Dragon
+function c999999927.matfilter(c)
+	return c:IsType(TYPE_MONSTER)
+end
+function c999999927.splimit(e,se,sp,st)
+	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+end
+function c999999927.spfilter(c,fc)
+	return c999999927.matfilter(c)
+end
+function c999999927.spcon(e,c)
+	if c==nil then return true end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local x=Duel.IsExistingMatchingCard(c999999927.spfilter,tp,LOCATION_ONFIELD,nil,2,nil,tp,ft)
+	Debug.Message("Start Cerberuscheck")
+	Debug.Message(ft)
+	Debug.Message(x)
+	return ft>-2--Min Count of Monsters
+		and x--the Number 2 represents the Min Count of Monsters
+end
+function c999999927.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetMatchingGroup(c999999927.spfilter,tp,LOCATION_MZONE,nil,nil,tp)
+	local g1=g:Select(tp,2,2,nil)--Count of Monsters thats needed. first is minimal count, second ist maximal count
+	Duel.SendtoGrave(g1,REASON_COST)
+end
+--Borrowed from Area A
+function c999999927.target(e,c)
+	return c:GetCode() == 999999927
+end
