b73b8273e3a9720bfec11e4636e648da793fa67c
NecroDraco <ygohameln@gmail.com>
2018-04-24 23:16:45 +0200

    Adding Elementsabers -- thanks to chevralyx

---
+++ script/c999999902.lua
@@ -0,0 +1,84 @@
+--エレメントセイバー・ラパウィラ
+--reused by chevalierx thx to allow me to use these cards
+function c999999902.initial_effect(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(999999902,0))
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c999999902.negcon)
+	e1:SetCost(c999999902.negcost)
+	e1:SetTarget(c999999902.negtg)
+	e1:SetOperation(c999999902.negop)
+	c:RegisterEffect(e1)
+	--att change
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(999999902,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c999999902.atttg)
+	e2:SetOperation(c999999902.attop)
+	c:RegisterEffect(e2)
+end
+function c999999902.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
+end
+function c999999902.costfilter(c)
+	return c:IsSetCard(0x400d) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
+end
+function c999999902.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local fg=Group.CreateGroup()
+	for i,pe in ipairs({Duel.IsPlayerAffectedByEffect(tp,61557074)}) do
+		fg:AddCard(pe:GetHandler())
+	end
+	local loc=LOCATION_HAND
+	if fg:GetCount()>0 then loc=LOCATION_HAND+LOCATION_DECK end
+	if chk==0 then return Duel.IsExistingMatchingCard(c999999902.costfilter,tp,loc,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tc=Duel.SelectMatchingCard(tp,c999999902.costfilter,tp,loc,0,1,1,nil):GetFirst()
+	if tc:IsLocation(LOCATION_DECK) then
+		local fc=nil
+		if fg:GetCount()==1 then
+			fc=fg:GetFirst()
+		else
+			fc=fg:Select(tp,1,1,nil)
+		end
+		Duel.Hint(HINT_CARD,0,fc:GetCode())
+		fc:RegisterFlagEffect(61557074,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,0)
+	end
+	Duel.SendtoGrave(tc,REASON_COST)
+end
+function c999999902.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function c999999902.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+function c999999902.atttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
+	local att=Duel.AnnounceAttribute(tp,1,0xff-e:GetHandler():GetAttribute())
+	e:SetLabel(att)
+end
+function c999999902.attop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e1:SetValue(e:GetLabel())
+		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
